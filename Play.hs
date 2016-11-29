767f6811b11608eb743c1da26137c5191aba7b8a
Sisi Guo <sisi.guo.123@gmail.com>
2016-11-28 18:10:43 -0800

    Add difficulty levels to play

--- Play.hs
+++ Play.hs
@@ -11,37 +11,60 @@ import Data.Char
 
 -- play game2048 initGame
 
+-- TODO: make these more realisitc!
+easyNumMoves = 20
+mediumNumMoves = 10
+hardNumMoves = 5
+
 play :: Game -> IO State -> IO ()
 play game state  = do
     putStrLn ("Welcome to the 2048 game!")
+    putStrLn ("What level do you want to play at? Input 'E' for Easy, 'M' for Medium, or 'H' for Hard.")
+    level <- getLine
+    case (toLower (level !! 0)) of
+        'e' -> putStrLn("You get " ++ show easyNumMoves ++ " moves to win the game.")
+        'm' -> putStrLn("You get " ++ show mediumNumMoves ++ " moves to win the game.")
+        'h' -> putStrLn("You get " ++ show hardNumMoves ++ " moves to win the game.")
     putStrLn ("This is your game board:")
     (startBoard,startNumMoves) <- state
     printBoard startBoard
     putStrLn ("To move, press 'U' for Up, 'D' for Down, 'L' for Left, and 'R' for Right.")
     putStrLn ("Input your first move:")
-    line <- getLine
-    playLoop game line (ContinueGame (startBoard,startNumMoves))
+    move <- getLine
+    playLoop game move level (ContinueGame (startBoard,startNumMoves))
 
-playLoop :: Game -> [Char] -> Result -> IO ()
-playLoop game move (ContinueGame (board,numMoves)) = do
-    case (toLower (move !! 0)) of
-        'u' -> playLoop_helper U
-        'd' -> playLoop_helper D
-        'l' -> playLoop_helper L
-        'r' -> playLoop_helper R
+playLoop :: Game -> [Char] -> [Char] -> Result -> IO ()
+playLoop game move level (ContinueGame (board,numMoves)) = case (toLower (level !! 0)) of
+    'e' -> if (numMoves == easyNumMoves)
+            then putStrLn ("You've exceeded the maximum number of moves for the Easy difficulty level. You lose :(")
+            else loop
+    'm' -> if (numMoves == mediumNumMoves)
+            then putStrLn ("You've exceeded the maximum number of moves for the Medium difficulty level. You lose :(")
+            else loop
+    'h' -> if (numMoves == hardNumMoves)
+            then putStrLn ("You've exceeded the maximum number of moves for the Hard difficulty level. You lose :(")
+            else loop
     where
-        playLoop_helper m = do
-            next <- game (Move m (board,numMoves))
-            case next of
-                (ContinueGame (b,n)) -> do
-                    printBoard b
-                    putStrLn("The number of moves you've taken: " ++ show n)
-                    putStrLn ("Input your next move:")
-                    line <- getLine
-                    playLoop game line next
-                (EndOfGame c) -> if (c == 1)
-                    then putStrLn ("You win!")
-                    else putStrLn ("No more moves. You lost :(") -- TODO: give option to restart?
+        loop = do
+            case (toLower (move !! 0)) of
+                'u' -> playLoop_helper U
+                'd' -> playLoop_helper D
+                'l' -> playLoop_helper L
+                'r' -> playLoop_helper R
+            where
+                playLoop_helper m = do
+                    next <- game (Move m (board,numMoves))
+                    case next of
+                        (ContinueGame (b,n)) -> do
+                            printBoard b
+                            putStrLn("The number of moves you've taken: " ++ show n)
+
+                            putStrLn ("Input your next move:")
+                            line <- getLine
+                            playLoop game line level next
+                        (EndOfGame c) -> if (c == 1)
+                            then putStrLn ("You win!")
+                            else putStrLn ("No more moves. You lose :(") -- TODO: give option to restart?
 
 printBoard :: Board -> IO ()
 printBoard board = do
