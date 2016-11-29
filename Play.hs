e14a663c452ad816d8928b7a6701d1406bd5fbdc
Sisi Guo <sisi.guo.123@gmail.com>
2016-11-28 17:05:08 -0800

    Incorporate "printBoard" into play game loop prompts; add move prompts

--- Play.hs
+++ Play.hs
@@ -16,8 +16,9 @@ play game state  = do
     putStrLn ("Welcome to the 2048 game!")
     putStrLn ("This is your game board:")
     startState <- state
-    putStrLn (show startState)
+    printBoard startState
     putStrLn ("To move, press 'U' for Up, 'D' for Down, 'L' for Left, and 'R' for Right.")
+    putStrLn ("Input your first move:")
     line <- getLine
     playLoop game line (ContinueGame startState)
 
@@ -33,7 +34,8 @@ playLoop game move (ContinueGame state) = do
             next <- game (Move m state)
             case next of
                 (ContinueGame s) -> do
-                    putStrLn (show next)
+                    printBoard s
+                    putStrLn ("Input your next move:")
                     line <- getLine
                     playLoop game line next
                 (EndOfGame c) -> if (c == 1)
